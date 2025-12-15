588dd619e812d26ac89076cb1fd84cbe2b36fddb
Obiwankenobi699 <mukulmishra795@gmail.com>
2025-12-15 18:08:21 +0500

    Feat:CI

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/CI-CD/",
   server:{
     port:3000,
     open:true
